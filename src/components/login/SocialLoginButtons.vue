58c6c0fd43ca94d583590a65dec1b857f5f360e9
sunghyun <shlim0287@naver.com>
2025-01-08 22:50:11 +0900

    [REFACTOR] 소셜 로그인 URL 설정 및 로그인 후 상태 체크

--- src/components/login/SocialLoginButtons.vue
+++ src/components/login/SocialLoginButtons.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="social-box" v-for="provider in socialProviders" :key="provider.id">
-      <button :id="provider.id" @click="loginWithProvider(provider.url)">
+      <button :id="provider.id" @click="loginWithProvider(provider)">
         <img :src="provider.icon" :alt="`${provider.name}_icon`" />
         {{ provider.label }}
       </button>
@@ -11,10 +11,18 @@
 
 <script>
 import "@/assets/css/socialLogin.css";
+import { useRouter } from 'vue-router';
+import { useUserStore } from "@/stores/userStore";
 
 export default {
   name: "SocialLoginButtons",
+  setup() {
+    const router = useRouter();
+    const userStore = useUserStore();
+    return { router, userStore };
+  },
   data() {
+    const baseUrl = 'http://localhost:8080';
     return {
       socialProviders: [
         {
@@ -22,30 +30,38 @@ export default {
           name: "google",
           label: "구글 계정으로 로그인",
           icon: require("@/assets/images/icon/icon_google.png"),
-          url: "/oauth2/authorization/google",
+          url: `${baseUrl}/oauth2/authorization/google`
         },
         {
           id: "naver-btn",
           name: "naver",
           label: "네이버 계정으로 로그인",
           icon: require("@/assets/images/icon/icon_naver.png"),
-          url: "/oauth2/authorization/naver",
+          url: `${baseUrl}/oauth2/authorization/naver`
         },
         {
           id: "kakao-btn",
           name: "kakao",
           label: "카카오 계정으로 로그인",
           icon: require("@/assets/images/icon/icon_kakao.png"),
-          url: "/oauth2/authorization/kakao",
+          url: `${baseUrl}/oauth2/authorization/kakao`
         },
       ],
     };
   },
   methods: {
-    loginWithProvider(url) {
-      window.location.href = url;
-    },
+    loginWithProvider(provider) {
+      window.location.href = provider.url;
+    }
   },
+  async mounted() {
+    if (window.location.search.includes('code=')) {
+      await this.userStore.checkSession();
+      if (this.userStore.isAuthenticated) {
+        this.router.push('/');
+      }
+    }
+  }
 };
 </script>
 
